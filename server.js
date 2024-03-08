47343331723ff805a4232750557f36876e95c545
Lorens Gueron <gueronlj@gmail.com>
2024-03-08 15:25:57 -0500

    Refactor: Tokens exchange

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
 const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
 
+let PUBLIC_TOKEN = null
+let ACCESS_TOKEN = null
+let ITEM_ID = null
+
 const user = { id: "USER_GOOD" }
 
 const configuration = new Configuration({
@@ -32,7 +36,7 @@ app.listen(3000, () => {
     console.log('Prject W is running on port 3000');
 });
 
-app.post('/api/create_link_token', async function (req, response) {
+app.post('/api/create_link_token', async function (req, res) {
     // Get the client_user_id by searching for the current user
     //const user = await User.find(...);
     const clientUserId = user.id;
@@ -50,34 +54,29 @@ app.post('/api/create_link_token', async function (req, response) {
     };
     try {
         const createTokenResponse = await client.linkTokenCreate(request);
-        const data = response.json(createTokenResponse.data);
-        console.log(data);
+        const data = res.json(createTokenResponse.data);
     } catch (error) {
         console.log(error);       
     }
 });
 
+//TODO: COMPLETE THE FOLLOWING ROUTES (exchange_public_token, set_access_token)
 app.post('/api/exchange_public_token', async function ( req, res, next) {
-    const publicToken = req.body.public_token;
+    PUBLIC_TOKEN = req.body.public_token;
     try {
-      const response = await client.itemPublicTokenExchange({
-        public_token: publicToken,
+      const tokenResponse = await client.itemPublicTokenExchange({
+        public_token: PUBLIC_TOKEN,
       });
-      // These values should be saved to a persistent database and
+        // These values should be saved to a persistent database and
       // associated with the currently signed-in user
-      // const accessToken = res.data.access_token;
-      // const itemID = res.data.item_id;
-      res.json({ public_token_exchange: 'complete' });
-      console.log('exchange success!', res.data);
+      ACCESS_TOKEN = tokenResponse.data.access_token;
+      ITEM_ID = tokenResponse.data.item_id;  
+      res.json({
+        // the 'access_token' is a private token, DO NOT pass this token to the frontend in your production environment
+        item_id: ITEM_ID,
+        error: null,
+      });
     } catch (error) {
-      console.log(error);
+      res.json({ error: error });
     }
 });
-
-app.post('/api/set_access_token', async function (req, res) {
-    const accessToken = req.body.public_token;
-    // In a production app, the access token should be persisted to a
-    // safe, long-term storage.
-    console.log('access token: ', accessToken);
-    res.json({ access_token_set: 'complete' });
-})
