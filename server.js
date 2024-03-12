d2ae3f2adb4443f0b927165be161489126e59722
Lorens Gueron <gueronlj@gmail.com>
2024-03-11 23:05:05 -0400

    Added: sorted transactions

--- server.js
+++ server.js
@@ -37,60 +37,59 @@ app.listen(3000, () => {
 });
 
 app.post('/api/create_link_token', async function (req, res) {
-    // Get the client_user_id by searching for the current user
-    //const user = await User.find(...);
-    const clientUserId = user.id;
-    const request = {
-        user: {
-        // This should correspond to a unique id for the current user.
-            client_user_id: clientUserId,
-        },
-        client_name: 'Plaid Test App',
-        products: ['auth'],
-        language: 'en',
-        webhook: 'https://webhook.example.com',
-        redirect_uri: 'https://localhost:5173/',
-        country_codes: ['US'],
-    };
-    try {
-        const createTokenResponse = await client.linkTokenCreate(request);
-        const data = res.json(createTokenResponse.data);
-    } catch (error) {
-        console.log(error);       
-    }
+  // Get the client_user_id by searching for the current user
+  //const user = await User.find(...);
+  const clientUserId = user.id;
+  const request = {
+      user: {
+      // This should correspond to a unique id for the current user.
+          client_user_id: clientUserId,
+      },
+      client_name: 'Plaid Test App',
+      products: ['auth'],
+      language: 'en',
+      webhook: 'https://webhook.example.com',
+      redirect_uri: 'https://localhost:5173/',
+      country_codes: ['US'],
+  };
+  try {
+      const createTokenResponse = await client.linkTokenCreate(request);
+      res.json(createTokenResponse.data);
+  } catch (error) {
+      console.log(error);       
+  }
 });
 
 //TODO: COMPLETE THE FOLLOWING ROUTES (exchange_public_token, set_access_token)
 app.post('/api/exchange_public_token', async function ( req, res) {
-    PUBLIC_TOKEN = req.body.public_token;
-    try {
-      const tokenResponse = await client.itemPublicTokenExchange({
-        public_token: PUBLIC_TOKEN,
-      });
-        // These values should be saved to a persistent database and
-      // associated with the currently signed-in user
-      ACCESS_TOKEN = tokenResponse.data.access_token;
-      ITEM_ID = tokenResponse.data.item_id;  
-      res.json({
-        // the 'access_token' is a private token, DO NOT pass this token to the frontend in your production environment
-        item_id: ITEM_ID,
-        error: null,
-      });
-    } catch (error) {
-      res.json({ error: error });
-    }
+  PUBLIC_TOKEN = req.body.public_token;
+  try {
+    const tokenResponse = await client.itemPublicTokenExchange({
+      public_token: PUBLIC_TOKEN,
+    });
+      // These values should be saved to a persistent database and
+    // associated with the currently signed-in user
+    ACCESS_TOKEN = tokenResponse.data.access_token;
+    ITEM_ID = tokenResponse.data.item_id;  
+    res.json({
+      // the 'access_token' is a private token, DO NOT pass this token to the frontend in your production environment
+      item_id: ITEM_ID,
+      error: null,
+    });
+  } catch (error) {
+    res.json({ error: error });
+  }
 });
 
 app.get('/api/accounts', async function (req, res) {
   try {
-      console.log("token:", ACCESS_TOKEN);
-      const accountsResponse = await client.accountsGet({
-        access_token: ACCESS_TOKEN,
-      });
-      res.json(accountsResponse.data);
-    } catch(error){
-      res.json({ error: error });      
-    }
+    const accountsResponse = await client.accountsGet({
+      access_token: ACCESS_TOKEN,
+    });
+    res.json(accountsResponse.data);
+  } catch(error){
+    res.json({ error: error.response.data.error_message });      
+  }
 });
 
 app.get('/api/liabilities', async function (req, res) {
@@ -98,9 +97,22 @@ app.get('/api/liabilities', async function (req, res) {
     const liabilitiesResponse = await client.liabilitiesGet({
       access_token: ACCESS_TOKEN,
     });
-    res.json({ error: null, liabilities: liabilitiesResponse.data });
+    res.json({ error: null, liabilities: liabilitiesResponse.data.liabilities });
   } catch(error){
-    res.json({ error: error });
+    res.json({ error: error.response.data.error_message });
+  } 
+})
+
+app.get('/api/transactions/recurring', async function (req, res) {
+  try{
+    const response = await client.transactionsRecurringGet({
+      access_token: ACCESS_TOKEN,
+    });
+    let incoming = response.data.inflowStreams;
+    let outgoing = response.data.outflowStreams;
+    res.json({ incoming: incoming, outgoing: outgoing });
+  }catch(error){
+    res.json({ error: error.response.data.error_message });
   }
 })
 
@@ -115,40 +127,75 @@ app.get('/api/balance', async function (req, response) {
   }
 });
 
-app.get('/api/transactions', async function (req, res) {
-  try{  
-    // Set cursor to empty to receive all historical updates
-    let cursor = null;
-    // New transaction updates since "cursor"
-    let added = [];
-    let modified = [];
-    // Removed transaction ids
-    let removed = [];
-    let hasMore = true;
-    // Iterate through each page of new transaction updates for item'
-    while (hasMore) {
-      const request = {
-        access_token: ACCESS_TOKEN,
-        cursor: cursor,
-      };
-      const response = await client.transactionsSync(request);
-      const data = response.data
-      console.log(data);
-      //store current page of results
-      added = added.concat(data.added);
-      modified = modified.concat(data.modified);
-      removed = removed.concat(data.removed);
-      hasMore = data.has_more;
-      //update cursor for next page
-      cursor = data.next_cursor;
-    }
-    res.json({
-      added: added, 
-      modified: modified, 
-      removed: removed, 
-      cursor: cursor 
+app.get('/api/investments', async function (req, res) {
+  try {
+    const response = await client.investmentsHoldingsGet({
+      access_token: ACCESS_TOKEN,
     });
-  }catch(error){
-    res.json({ error: error });
+    const holdings = response.data.holdings;
+    const securities = response.data.securities;
+    res.json({holdings: holdings, securities: securities});
+  } catch (error) {
+    console.log(error.response.data.error_message);
+    res.json({ error: error.response.data.error_message });
   }
+})
+
+app.get('/api/item', async function (req, res) {
+  try {
+    const response = await client.itemGet({
+      access_token: ACCESS_TOKEN,
+    });
+    const item = response.data.item;
+    const status = response.data.status;
+    res.json({ item: item, status: status });
+  } catch (error) {
+    res.json({ error: error.response.data.error_message })
+  }
+})
+
+app.get('/api/transactions', function (request, response) {
+  Promise.resolve()
+    .then(async function () {
+      // Set cursor to empty to receive all historical updates
+      let cursor = null;
+
+      // New transaction updates since "cursor"
+      let added = [];
+      let modified = [];
+      // Removed transaction ids
+      let removed = [];
+      let hasMore = true;
+      // Iterate through each page of new transaction updates for item
+      while (hasMore) {
+        const request = {
+          access_token: ACCESS_TOKEN,
+          cursor: cursor,
+        };
+        const response = await client.transactionsSync(request)
+        const data = response.data;
+        // Add this page of results
+        added = added.concat(data.added);
+        modified = modified.concat(data.modified);
+        removed = removed.concat(data.removed);
+        hasMore = data.has_more;
+        // Update cursor to the next cursor
+        cursor = data.next_cursor;
+      }
+
+      const compareTxnsByDateAscending = (a, b) => (a.date < b.date) - (a.date > b.date);
+      // Return the 8 most recent transactions
+      const recently_added = [...added].sort(compareTxnsByDateAscending);
+
+      response.json({
+        added: recently_added,
+        modified: modified,
+        removed: removed,
+        hasMore: hasMore,
+      });
+    })
+    .catch((error) => {
+      console.log(error.response.data.error_message);
+      response.json({ error: error.response.data.error_message });
+    });
 });
