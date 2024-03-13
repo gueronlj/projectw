a43af35dcf9953ad85cb50cdd0c8d79d9082bba9
Lorens Gueron <gueronlj@gmail.com>
2024-03-12 22:56:46 -0400

    WIP: capturing current user and access token

--- server.js
+++ server.js
@@ -17,14 +17,14 @@ let ITEM_ID = null
 const user = { id: "USER_GOOD" }
 
 const configuration = new Configuration({
-    basePath: PlaidEnvironments[PLAID_ENV],
-    baseOptions: {
-      headers: {
-        'PLAID-CLIENT-ID': PLAID_CLIENT_ID,
-        'PLAID-SECRET': PLAID_SECRET,
-        'Plaid-Version': '2020-09-14',
-      },
+  basePath: PlaidEnvironments[PLAID_ENV],
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': PLAID_CLIENT_ID,
+      'PLAID-SECRET': PLAID_SECRET,
+      'Plaid-Version': '2020-09-14',
     },
+  },
 });
 
 const client = new PlaidApi(configuration);
@@ -33,34 +33,35 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log('Prject W is running on port 3000');
+  console.log('Prject W is running on port 3000');
 });
 
 app.post('/api/create_link_token', async function (req, res) {
   // Get the client_user_id by searching for the current user
   //const user = await User.find(...);
-  const clientUserId = user.id;
+  const clientUserId = req.body.user_id;
   const request = {
-      user: {
-      // This should correspond to a unique id for the current user.
-          client_user_id: clientUserId,
-      },
-      client_name: 'Plaid Test App',
-      products: ['auth'],
-      language: 'en',
-      webhook: 'https://webhook.example.com',
-      redirect_uri: 'https://localhost:5173/',
-      country_codes: ['US'],
+    user: {
+    // This should correspond to a unique id for the current user.
+      client_user_id: clientUserId,
+    },
+    client_name: 'Plaid Test App',
+    products: ['auth'],
+    language: 'en',
+    webhook: 'https://webhook.example.com',
+    redirect_uri: 'https://localhost:5173/',
+    country_codes: ['US'],
   };
   try {
-      const createTokenResponse = await client.linkTokenCreate(request);
-      res.json(createTokenResponse.data);
+    const createTokenResponse = await client.linkTokenCreate(request);
+    res.json(createTokenResponse.data);
+    console.log(createTokenResponse.data);
   } catch (error) {
-      console.log(error);       
+    console.log(error);       
   }
 });
 
-//TODO: COMPLETE THE FOLLOWING ROUTES (exchange_public_token, set_access_token)
+
 app.post('/api/exchange_public_token', async function ( req, res) {
   PUBLIC_TOKEN = req.body.public_token;
   try {
